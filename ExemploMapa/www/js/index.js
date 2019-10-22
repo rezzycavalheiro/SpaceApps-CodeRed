958242be155b2dbb446dd96ec97e161af903b2ec
rodrigoorf <rodrigoorf@users.noreply.github.com>
2019-10-22 20:40:58 -0300

    Última versão

--- ExemploMapa/www/js/index.js
+++ ExemploMapa/www/js/index.js
@@ -1,13 +1,18 @@
 ons.disableAutoStyling();
+ons.disableIconAutoPrefix();
     var map;
+    var mapSetLocation;
     document.addEventListener("deviceready", function() {
       var div = document.getElementById("map_canvas");
-      document.getElementById("teste").style.marginLeft = "80%";
+      var msl = document.getElementById("map_canvas_report");
+      document.getElementById("teste").style.marginLeft = "10%";
 
       // Create a Google Maps native view under the map_canvas div.
       map = plugin.google.maps.Map.getMap(div);
+      mapSetLocation = plugin.google.maps.Map.getMap(msl);
 
       map.one(plugin.google.maps.event.MAP_READY, onMapInit);
+      mapSetLocation.one(plugin.google.maps.event.MAP_READY, onMapSetLocationInit);
 
     }, false);
 
@@ -23,8 +28,7 @@ ons.disableAutoStyling();
 
             var marcador = map.addMarker({
                 position: { lat: localizacao.coords.latitude, lng: localizacao.coords.longitude },
-                title: "Você está aqui",
-                snippet: "E eu não estou brincando",
+                title: "You are here",
                 //icon: image,
                 animation: plugin.google.maps.Animation.BOUNCE
             });
@@ -73,4 +77,45 @@ ons.disableAutoStyling();
             console.log("deu ruim :(");
         })
 
+    }
+
+    function onMapSetLocationInit(){
+        navigator.geolocation.getCurrentPosition(function (localizacao) {
+            mapSetLocation.moveCamera({
+                target: { lat: localizacao.coords.latitude, lng: localizacao.coords.longitude },
+                zoom: 17
+                //tilt: 60,
+                //bearing: 140,
+                //duration: 5000
+            });
+
+            var marcador = mapSetLocation.addMarker({
+                position: { lat: localizacao.coords.latitude, lng: localizacao.coords.longitude },
+                draggable: true,
+                //icon: image,
+                animation: plugin.google.maps.Animation.BOUNCE
+            });
+        }, console.error);
+    }
+
+    function addPicture(){
+        navigator.camera.getPicture(function(success){
+            var image = document.getElementById('miniaturaImagem');
+            image.src = "data:image/jpeg;base64," + success;
+            console.log(success);
+        }, function(error){
+            console.log(error);
+        }, {
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+    }
+
+    function reportFire(){
+        ons.notification.alert({
+            message: "Fire report sent successfully!",
+            title: "Report"
+        });
+
+        var image = document.getElementById('miniaturaImagem');
+        image.src = "";
     }
